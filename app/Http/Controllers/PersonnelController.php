1a8687962aab428a0c65662a8991b7548348dfa7
Foudja Laeti <foudjalaeti@gmail.com>
2026-03-20 18:04:36 -0700

    fix: correct Rule::unique ignore() in PersonnelController update

--- app/Http/Controllers/PersonnelController.php
+++ app/Http/Controllers/PersonnelController.php
@@ -82,8 +82,8 @@ class PersonnelController extends Controller
         $validateData = $request->validate([
             'nom_pers'   => 'sometimes|string',
             'sexe_pers'  => 'sometimes|string|in:Masculin,Feminin',
-            'phone_pers' => ['sometimes', 'string', Rule::unique('personnels', 'phone_pers')->ignore($personnel->id, 'id')],
-            'login_pers' => ['sometimes', 'string', Rule::unique('personnels', 'login_pers')->ignore($personnel->id, 'id')],
+            'phone_pers' => ['sometimes', 'string', Rule::unique('personnels', 'phone_pers')->ignore($personnel->getKey(), $personnel->getKeyName())],
+            'login_pers' => ['sometimes', 'string', Rule::unique('personnels', 'login_pers')->ignore($personnel->getKey(), $personnel->getKeyName())],
             'pwd_pers'   => 'sometimes|string',
             'type_pers'  => 'sometimes|string',
         ]);
