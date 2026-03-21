40e81e0e31f1a466cc8b2d55917d67c679533f84
Foudja Laeti <foudjalaeti@gmail.com>
2026-03-21 12:26:34 -0700

    style: fix linting in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Spatie\LaravelPrometheus\Http\Controllers\MetricsController;
 
 Route::get('/', function () {
     return view('filiere');
@@ -13,7 +12,7 @@ Route::get('/metrics', function () {
     // Temps de réponse simulé
     $metrics[] = '# HELP http_response_time_seconds Response time';
     $metrics[] = '# TYPE http_response_time_seconds gauge';
-    $metrics[] = 'http_response_time_seconds ' . round(microtime(true) - LARAVEL_START, 4);
+    $metrics[] = 'http_response_time_seconds '.round(microtime(true) - LARAVEL_START, 4);
 
     // Taux d'erreur
     $metrics[] = '# HELP http_errors_total Total HTTP errors';
@@ -23,7 +22,7 @@ Route::get('/metrics', function () {
     // Mémoire utilisée
     $metrics[] = '# HELP php_memory_usage_bytes Memory usage';
     $metrics[] = '# TYPE php_memory_usage_bytes gauge';
-    $metrics[] = 'php_memory_usage_bytes ' . memory_get_usage(true);
+    $metrics[] = 'php_memory_usage_bytes '.memory_get_usage(true);
 
     return response(implode("\n", $metrics))
         ->header('Content-Type', 'text/plain; version=0.0.4');
