de2e151e061662c70bf19c49e85ce787f9aaf148
Foudja Laeti <foudjalaeti@gmail.com>
2026-04-24 13:54:47 -0700

    fix: allow CORS for frontend localhost and production

--- config/cors.php
+++ config/cors.php
@@ -1,14 +1,23 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'filiere', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // tu peux remplacer par http://localhost:4200
+
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://foudja.cdwfs.net',
+    ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => true,
 ];
-
-// ISO9126 : Norme de definition d'un logiciel
